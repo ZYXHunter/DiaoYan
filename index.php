581e6b64ac59387fb1a463b77fe13d28de94380f
paxwang <paxwang@qq.com>
2015-10-24 20:37:14 +0800

    main

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="LiuxueGo">
+    <meta name="keyword" content="LiuxueGo">
+    <link rel="stylesheet" href="main.css">
+    <title>留学GO游学项目</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ program2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pax
+ * Date: 15/10/24
+ * Time: 20:35
+ */ 
